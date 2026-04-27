5ec8cf7dd4cac49eac154d33bc97e99d3bd5ddf2
Nikhil0xSec <devnikhil19@gmail.com>
2026-04-27 15:05:58 +0530

    QuantityMeasurementApp - UC2: Inches measurement equality Completed

--- APP/src/QuantityMeasurementApp.java
+++ APP/src/QuantityMeasurementApp.java
@@ -1,22 +1,27 @@
 /***
- * QuantityMeasurementApp - UC1: Feet measurement equality
+ * QuantityMeasurementApp - UC2: Inches measurement equality
  *
  * This class is responsible for checking the equality of two numerical values
  * measured in feet in the quantity Measurement Application.
  *
  */
-
 public class QuantityMeasurementApp {
-    // Inner class to represent Feet measurement
 
+    // Inner class to represent Feet measurement
     public static class Feet {
+
         private final double value;
+
         public Feet(double value) {
             this.value = value;
         }
 
+        /*
+         * Override equals() for Feet comparison
+         */
         @Override
         public boolean equals(Object obj) {
+
             if (this == obj) {
                 return true;
             }
@@ -34,11 +39,67 @@ public class QuantityMeasurementApp {
             return Double.compare(this.value, feet.value) == 0;
         }
     }
-    // main class
-    public static void main(String[] args) {
+
+    // Inner class to represent Inches measurement
+    public static class Inches {
+
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        /*
+         * Override equals() for Inches comparison
+         */
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null) {
+                return false;
+            }
+
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+
+            Inches inches = (Inches) obj;
+
+            return Double.compare(this.value, inches.value) == 0;
+        }
+    }
+
+    /*
+     * Static method to demonstrate Feet equality
+     */
+    public static void demonstrateFeetEquality() {
         Feet feet1 = new Feet(1.0);
         Feet feet2 = new Feet(1.0);
 
-        System.out.println("Equal: " + feet1.equals(feet2));
+        System.out.println("Feet Equal: " + feet1.equals(feet2));
+    }
+
+    /*
+     * Static method to demonstrate Inches equality
+     */
+    public static void demonstrateInchesEquality() {
+        Inches inch1 = new Inches(1.0);
+        Inches inch2 = new Inches(1.0);
+
+        System.out.println("Inches Equal: " + inch1.equals(inch2));
+    }
+
+    /*
+     * Main method
+     */
+    public static void main(String[] args) {
+
+        demonstrateFeetEquality();
+
+        demonstrateInchesEquality();
     }
 }
--- APP/src/QuantityMeasurementAppTest.java
+++ APP/src/QuantityMeasurementAppTest.java
@@ -1,8 +1,5 @@
-
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
 
 public class QuantityMeasurementAppTest {
 
@@ -42,5 +39,45 @@ public class QuantityMeasurementAppTest {
         QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
 
         assertTrue(feet1.equals(feet1));
+    }
+
+    // ---------- Inches Test Cases ----------
+
+    @Test
+    public void testInchesEquality_SameValue() {
+        QuantityMeasurementApp.Inches inch1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches inch2 = new QuantityMeasurementApp.Inches(1.0);
+
+        assertTrue(inch1.equals(inch2));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue() {
+        QuantityMeasurementApp.Inches inch1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches inch2 = new QuantityMeasurementApp.Inches(2.0);
+
+        assertFalse(inch1.equals(inch2));
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison() {
+        QuantityMeasurementApp.Inches inch1 = new QuantityMeasurementApp.Inches(1.0);
+
+        assertFalse(inch1.equals(null));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentClass() {
+        QuantityMeasurementApp.Inches inch1 = new QuantityMeasurementApp.Inches(1.0);
+        String value = "1.0";
+
+        assertFalse(inch1.equals(value));
+    }
+
+    @Test
+    public void testInchesEquality_SameReference() {
+        QuantityMeasurementApp.Inches inch1 = new QuantityMeasurementApp.Inches(1.0);
+
+        assertTrue(inch1.equals(inch1));
     }
 }
